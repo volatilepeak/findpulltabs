d9ca2973428f8ab304ff9d67a3933da827bbedd4
volatilepeak <volatilepeakllc@gmail.com>
2026-05-08 14:25:25 -0500

    Add pull tab favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -58,7 +58,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         <link
           rel="stylesheet"
           href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
